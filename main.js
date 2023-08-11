78ed74357996923cd1bf4e1b8fea3de6ee4046b5
TechMasterApp <84641823+TechMasterApp@users.noreply.github.com>
2023-08-11 10:51:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ noseY = 0
 function preload() {
     clownNose = loadImage("https://i.postimg.cc/jq1L1nh3/clownnose.png")
     clownHat = loadImage("https://i.postimg.cc/vm64bYHL/clownhat.png")
+    clownFrill = loadImage("https://i.postimg.cc/2jT4mtNT/891-8919492-mini-costume-chiffon-striped-ruffle-removebg-preview.png")
 }
 
 function setup() {
-    canvas = createCanvas(700, 525)
-    canvas.center()
+    canvas = createCanvas(1000, 725)
+    canvas.position()
     video = createCapture(VIDEO)
     video.hide()
     posenet = ml5.poseNet(video, modelLoaded)
@@ -17,9 +18,10 @@ function setup() {
 }
 
 function draw() {
-    image(video, 0, 0, 700, 525)
-    image(clownNose, noseX, noseY, 75, 75)
-    image(clownHat, noseX - 250, noseY - 500, 600, 400)
+    image(video, 0, 0, 1000, 725)
+    image(clownNose, noseX + 100, noseY + 50, 125, 125)
+    image(clownHat, noseX - 175, noseY - 400, 600, 400)
+    image(clownFrill, noseX - 150, noseY + 100, 600, 500)
 }
 
 function modelLoaded() {
